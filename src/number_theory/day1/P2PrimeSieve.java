976f214fc051dabbd0610d61d0ba59fb5a0cfa54
Jayant <ukeyjayant@gmail.com>
2024-04-03 23:41:32 +0530

    add comments to the code

--- src/number_theory/day1/P2PrimeSieve.java
+++ src/number_theory/day1/P2PrimeSieve.java
@@ -9,13 +9,17 @@ public class P2PrimeSieve {
 	public static boolean[] primeSieve(int n) {
 		boolean isPrime[] = new boolean[n+1];
 		
-		Arrays.fill(isPrime, true);
-		isPrime[0] = isPrime[1] = false;		
-		for(int i=2; i*i<=n; i++) {
-			if(!isPrime[i])
+		Arrays.fill(isPrime, true); // Filling intitally all the array values as a true
+		
+		isPrime[0] = isPrime[1] = false;	// 0 and 1 are falling under prime category so mark it as false
+		
+		for(int i=2; i*i<=n; i++) {			// Running loop till square root of n, as after that if we try, then we will found that it was already multiple of someone
+			if(!isPrime[i])	// If number is not-prime then we are continuing the loop
 				continue;
 			
-			for(int j=i*i; j<=n; j+=i) {
+			for(int j=i*i; j<=n; j+=i) { // increment j by i value
+										// Instead of i*2 ,we are doing i*i// as the composite number is divisible by smallest prime number
+										//To understand it better-ly u can dry run by taking n value as 30. 
 				isPrime[j] = false;
 			}
 		}
